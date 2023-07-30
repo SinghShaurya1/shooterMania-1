c0802f0e24b14894014758adc348f44c0f0260b3
SinghShaurya1 <104992391+SinghShaurya1@users.noreply.github.com>
2023-07-30 12:04:34 +0530

    Add files via upload

---
+++ game-boilerplate-latest-main/sketch.js
@@ -0,0 +1,22 @@
+var splash,playbutton,nextbutton,soundbutton,nosoundbutton
+var gameState="wait"
+
+
+function preload(){
+    splash=loadImage("splash.gif")
+
+
+}
+
+function setup(){
+createCanvas(windowWidth,windowHeight)
+
+}
+
+function draw(){
+background(splash)
+
+
+}
+
+//anjali.kapur@byjusteachers.com
